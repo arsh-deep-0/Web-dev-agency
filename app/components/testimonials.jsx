17d83232830a1ffe96c6d9ae4d91cf37be961571
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-12-23 18:14:46 +0530

    timeline gray

--- app/components/testimonials.jsx
+++ app/components/testimonials.jsx
@@ -1,6 +1,6 @@
 function Testimonials (){
     return <>
-    <div className=" flex justify-center py-4">
+    <div className=" flex justify-center pt-4">
     <img src="testimonial-people.svg" alt="trustpilot reviews ratings" />
     </div>
    
