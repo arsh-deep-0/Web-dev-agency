6ce324de69f4527d31d2e3d534a7162c3c18c508
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-12-23 18:03:22 +0530

    .

--- app/components/testimonials.jsx
+++ app/components/testimonials.jsx
@@ -1,7 +1,7 @@
 function Testimonials (){
     return <>
     <div className=" flex justify-center py-4">
-    <img src="testimonials.svg" alt="trustpilot reviews ratings" />
+    <img src="testimonial-people.svg" alt="trustpilot reviews ratings" />
     </div>
    
     </>
