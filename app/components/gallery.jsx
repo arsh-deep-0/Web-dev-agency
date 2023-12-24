54984a23e5c3ae3dcb0370490b8cc0c684bb1ac2
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-12-24 12:43:55 +0530

    checking gsap

--- app/components/gallery.jsx
+++ app/components/gallery.jsx
@@ -1,4 +1,27 @@
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 function Gallery() {
+    gsap.registerPlugin(ScrollTrigger);
+  
+
+    useEffect(() => {
+        gsap.from(".kitchen", {
+            opacity:50,
+            y:50,
+            
+            stagger: 0.2,
+            ease: 'sine.Out',
+            scrollTrigger: {
+                trigger: ".gallery-container",
+                start: "top center",
+               markers: true,
+                toggleActions: "restart reverse restart reverse ",
+            }
+        }
+        );
+    },[]);
+
     return <div className="justify-center  flex w-full">
         <div className="flex gallery-container items-center justify-center">
             <div>
@@ -16,18 +39,18 @@ function Gallery() {
                         <img src="gallery-description.svg" alt="" />
                     </div>
                 </div>
-                <div className=" flex ">
+                <div className="kitchen-container flex ">
                     <div>
-                        <img src="kitchen-1.svg" alt="" />
+                        <img className="kitchen" src="kitchen-1.svg" alt="" />
                     </div>
                     <div>
-                        <img src="kitchen-2.svg" alt="" />
+                        <img className="kitchen" src="kitchen-2.svg" alt="" />
                     </div>
                     <div>
-                        <img src="kitchen-3.svg" alt="" />
+                        <img className="kitchen" src="kitchen-3.svg" alt="" />
                     </div>
                     <div>
-                        <img src="kitchen-4.svg" alt="" />
+                        <img className="kitchen" src="kitchen-4.svg" alt="" />
                     </div>
                 </div>
 
--- app/components/reviewsbox.jsx
+++ app/components/reviewsbox.jsx
@@ -1,7 +1,19 @@
 function ReviewsBox (){
     return <>
-    <div className=" flex justify-center py-4">
-    <img src="ReviewsBox.svg" alt="trustpilot reviews ratings" />
+    <div className=" flex justify-center py-4  w-full">
+        <div className="  flex items-center justify-center">
+            <p className="absolute  review-text w-3/5 ml-6 text-center mt-2 text-sm crimson">  These Guys provide excellent service at premium rates.</p>
+            <div className="absolute  flex items-center justify-center bg-black rounded-full h-16 w-16 review-photo">
+                <img className="w-full" src="happy.svg" alt="" />
+            </div>
+            <p className="client-name absolute text-sm  crimson ">Garry <br /><span className="client-role encodesans text-xs" >Business Owner</span></p>
+
+            
+            <p className="client-business absolute text-xs w-36 text-center ebgara">UNIVERSAL CUSTOM KITCHENS</p>
+        <img  src="reviews-box.svg" alt="trustpilot reviews ratings" />
+           
+        </div>
+
     </div>
    
     </>
