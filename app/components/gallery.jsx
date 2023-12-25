2cd357b70f8278fcc9c765c22fd460a0b0457ae0
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-12-25 22:09:06 +0530

    .

--- app/components/gallery.jsx
+++ app/components/gallery.jsx
@@ -35,7 +35,7 @@ function Gallery() {
                 trigger: ".gallery-container",
                 start: "top center+=290",
                 end:"top center+=80",
-                scrub: true,
+                scrub: 1,
                  toggleActions: "restart none restart none ",
               
             }
