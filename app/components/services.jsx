4471c696919f7a7cc9837df7f6534751b7e67cf1
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-12-25 22:11:12 +0530

    .

--- app/components/services.jsx
+++ app/components/services.jsx
@@ -1,22 +1,22 @@
 function Services(){
  return <div className="p-4 flex-col gap-4 flex">
  <div className="bg-white rounded-sm funnel-piece">
- <p className="blue-text text-center py-1 encodesans">Website development</p>
+ <p className="blue-text text-center py-1 font-bold encodesans">Website development</p>
  </div>
  <div className="bg-white rounded-sm funnel-piece">
- <p className="blue-text text-center py-1 encodesans">Responsive web Design</p>
+ <p className="blue-text text-center py-1 font-bold encodesans">Responsive web Design</p>
  </div>
  <div className="bg-white rounded-sm funnel-piece">
- <p className="blue-text text-center py-1 encodesans">Inventory Management Systems</p>
+ <p className="blue-text text-center py-1 font-bold encodesans">Inventory Management Systems</p>
  </div>
  <div className="bg-white rounded-sm funnel-piece">
- <p className="blue-text text-center py-1 encodesans">E-commerce Solutions</p>
+ <p className="blue-text text-center py-1 font-bold encodesans">E-commerce Solutions</p>
  </div>
  <div className="bg-white rounded-sm funnel-piece">
- <p className="blue-text text-center py-1 encodesans">Content Management Systems</p>
+ <p className="blue-text text-center py-1 font-bold encodesans">Content Management Systems</p>
  </div>
  <div className="bg-white rounded-sm funnel-piece">
- <p className="blue-text text-center py-1 encodesans">Web Hosting and Maintaince</p>
+ <p className="blue-text text-center py-1 font-bold encodesans">Web Hosting and Maintaince</p>
  </div>
     
  </div>  
