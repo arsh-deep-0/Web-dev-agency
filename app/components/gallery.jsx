0adb915951da86691950eea6bcee581cde39308a
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-12-25 23:16:44 +0530

    .

--- app/components/gallery.jsx
+++ app/components/gallery.jsx
@@ -43,7 +43,7 @@ function Gallery() {
         );
     },[]);
 
-    return <div className="justify-center  flex w-full">
+    return <div className="justify-center  flex w-full overflow-hidden">
         <div className="flex gallery-container items-center justify-center overflow-hidden">
             <div>
                 <img className="kitchen" src="side handle.svg" alt="" />
