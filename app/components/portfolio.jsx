aa8d075e9c383cab85785afd9cfdabdafbfbbcd3
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-12-25 18:40:37 +0530

    .

--- app/components/portfolio.jsx
+++ app/components/portfolio.jsx
@@ -52,8 +52,8 @@ function Portfolio() {
     //     };
     // }, []);
     return <>
-        <div className=" bg-black py-8 px-2 overflow-hidden">
-            <h2 className="text-white text-center text-3xl aoboshi">Recent Work</h2>
+        <div className=" bg-black py-8 px-2 overflow-hidden black-wire">
+            <h2 className="text-white text-center text-3xl aoboshi blue-text">Recent Work</h2>
             <div  className=" flex items-center text-white">
                 <div className="flex-col items-center py-8 gap-0 ">
                     <img className="" src=" project-1.svg" alt="" />
--- app/components/pricing.jsx
+++ app/components/pricing.jsx
@@ -1,8 +1,9 @@
 function Pricing (){
     return <>
     <div className="flex-col bg-white  py-8 px-2 ">
-        <h2 className="text-center text-4xl mb-8 blue-text aoboshi ">Pricing levels</h2>
-        <div className="flex flex-col bg-black items-center justify-center  rounded-md py-4 text-white my-2">
+        <h2 className="text-center text-4xl  blue-text aoboshi h-12 underline">Pricing levels</h2>
+      
+        <div className="flex flex-col bg-black items-center justify-center  rounded-md py-4 text-white my-2 mt-8">
             <p className="rose text-center text-3xl">Standard</p>
             <p>Best for small businesses</p>
             <p className="aoboshi text-3xl mt-4">$699</p>
