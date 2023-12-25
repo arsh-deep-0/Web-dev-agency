06323b78b79c95425c0e01434e3ffce59c189e44
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-12-25 21:42:14 +0530

    .

--- app/components/reviewsbox.jsx
+++ app/components/reviewsbox.jsx
@@ -12,64 +12,52 @@ function ReviewsBox() {
         "Spectacular design work! The user interface is intuitive, and the site's responsiveness is top-notch. Impressed beyond words."];
     const clients = ["Diego Ramirez", "Dalwinder Singh", "Yuki Tanaka ", "Jacob Thompson", "Oliver Dufour"];
 
-    const businesses = ["UNNIVERSAL  KITCHENS", "RENOVATEXPERTS", "UNNITRICK", "SPEEDSCALEZ", "REMOELRISE"];
+    const businesses = ["UNIVERSAL KITCHENS", "RENOVATEXPERTS", "UNITRICK", "SPEEDSCALEZ", "REMOELRISE"];
 
     const roles = [" Business Owner", " Business Manager", " Lead Remodeler", " Coordinator", " Owner"];
 
     const [index, setIndex] = useState(0);
-    const [imageSrc, setImageSrc] = useState(photos[index]);
-    const [reviewText, setreviewText] = useState(reviews[index]);
-    const [clientName, setclientName] = useState(clients[index]);
-    const [clientRole, setclientRole] = useState(roles[index]);
-    const [clientBusiness, setclientBusiness] = useState(businesses[index]);
+    const [imageSrc, setImageSrc] = useState(photos[4]);
+    const [reviewText, setreviewText] = useState(reviews[4]);
+    const [clientName, setclientName] = useState(clients[4]);
+    const [clientRole, setclientRole] = useState(roles[4]);
+    const [clientBusiness, setclientBusiness] = useState(businesses[4]);
 
     useEffect(() => {
-
-        const reduce = () => {
-            console.log('reduce')
-            gsap.to('.review', {
-                scale: 0,
-                duration: 500,
-            })
-        }
-
-        const expand = () => {
-            console.log('expand')
-            gsap.to('.review', {
-                delay: 500,
-                scale: 1,
-                duration: 500,
-            })
-        }
         const interval = setInterval(() => {
             setIndex(prevIndex => (prevIndex + 1) % photos.length);
-            console.log(index);
-            reduce();
-            setImageSrc(photos[index]);
-            setreviewText(reviews[index]);
-            setclientName(clients[index]);
-            setclientBusiness(businesses[index]);
-            setclientRole(roles[index]);
-            expand();
+
+            gsap.to('.review', {
+                scale: 0.5,
+                duration: 0.3,
+                onComplete: () => {
+                    setImageSrc(photos[index]);
+                    setreviewText(reviews[index]);
+                    setclientName(clients[index]);
+                    setclientBusiness(businesses[index]);
+                    setclientRole(roles[index]);
+
+                    gsap.to('.review', {
+                        scale: 1,
+                        duration: 0.4,
+                    });
+                },
+            });
         }, 4000);
 
-
-
-
-
         return () => clearInterval(interval); // Clean up the interval           
-    },)
+    }, [index, photos, reviews, clients, businesses, roles]);
     return <>
         <div className=" flex justify-center py-4 mb-12  pl-4  w-full ">
             <div className="  flex items-center justify-center">
-                <p className="review absolute  review-text w-4/6 ml-6 text-center mt-2 text-sm crimson">{reviewText}</p>
+                <p className="review absolute  review-text w-4/6  text-center  text-sm crimson">{reviewText}</p>
                 <div className="review absolute  flex items-center justify-center bg-white rounded-full h-16 w-16 review-photo bg-blue-voilet">
                     <img className="w-full" src={imageSrc} alt="" />
                 </div>
                 <p className="review client-name absolute text-sm  crimson ">{clientName} <br /><span className="client-role encodesans text-xs " >{clientRole}</span></p>
 
 
-                <p className="review client-business absolute text-xs w-36 text-center ebgara blue-text">{clientBusiness}</p>
+                <p className="review client-business absolute text-xs w-36  ebgara blue-text">{clientBusiness}</p>
                 <img src="reviews-box.svg" alt="trustpilot reviews ratings" />
 
             </div>
