0898f3d352be0640d21f910bc932f2254248ed40
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-12-25 18:56:58 +0530

    .

--- app/components/services.jsx
+++ app/components/services.jsx
@@ -1,21 +1,21 @@
 function Services(){
  return <div className="p-4 flex-col gap-4 flex">
- <div className="bg-white rounded-md funnel-piece">
+ <div className="bg-white rounded-sm funnel-piece">
  <p className="blue-text text-center py-1 encodesans">Website development</p>
  </div>
- <div className="bg-white rounded-md funnel-piece">
+ <div className="bg-white rounded-sm funnel-piece">
  <p className="blue-text text-center py-1 encodesans">Responsive web Design</p>
  </div>
- <div className="bg-white rounded-md funnel-piece">
+ <div className="bg-white rounded-sm funnel-piece">
  <p className="blue-text text-center py-1 encodesans">Inventory Management Systems</p>
  </div>
- <div className="bg-white rounded-md funnel-piece">
+ <div className="bg-white rounded-sm funnel-piece">
  <p className="blue-text text-center py-1 encodesans">E-commerce Solutions</p>
  </div>
- <div className="bg-white rounded-md funnel-piece">
+ <div className="bg-white rounded-sm funnel-piece">
  <p className="blue-text text-center py-1 encodesans">Content Management Systems</p>
  </div>
- <div className="bg-white rounded-md funnel-piece">
+ <div className="bg-white rounded-sm funnel-piece">
  <p className="blue-text text-center py-1 encodesans">Web Hosting and Maintaince</p>
  </div>
     
