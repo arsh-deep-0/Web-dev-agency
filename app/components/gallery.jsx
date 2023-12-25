077143beea55054122db2c48408fbefa5164b0a8
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-12-25 18:50:29 +0530

    .

--- app/components/gallery.jsx
+++ app/components/gallery.jsx
@@ -10,7 +10,7 @@ function Gallery() {
             opacity:0,
             x:25,
             
-            stagger: 0.15,
+            stagger: 0.1,
             ease: 'linear',
             scrollTrigger: {
                 trigger: ".gallery-container",
