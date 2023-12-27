f7453ed09bac2470d8759bbcbd1e276281bef9c6
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-12-28 02:38:09 +0530

    .

--- app/components/splashscreen.jsx
+++ app/components/splashscreen.jsx
@@ -10,8 +10,9 @@ function SplashScreen() {
         // Define the animation timeline
 
         tl.to('.box', { duration: 0.5, y: -400, ease: 'back.inOut' })
-          .to('.box', { duration:0.5, width:220, ease: 'back.inOut' }) 
+          .to('.box', { duration:0.5, width:480, ease: 'back.inOut'}) 
           .to('.box-text',{duration:0.5, opacity:1, ease: 'back.inOut'}, "<")
+        
           .to('.box-subtext',{duration:0.5, opacity:1, ease: 'back.inOut'}, ">")
           .to('.splash', { opacity: 0, duration: 0.2, onComplete: () => { setShowSplash(false); } }, "+=5");
           
@@ -22,10 +23,10 @@ function SplashScreen() {
     }, []);
 
     return (
-        <div className={` splash flex flex-col items-center justify-end ${showSplash ? '' : 'splashHidden'}`}>
-            <div className='box bg-white h-12 w-12 rounded-sm flex items-center justify-center p-2'>
-                <img className='box-text opacity-0 h-8' src="logo.svg" alt="" />
-                <p className='box-text text-center blue-text opacity-0 text-lg bodoni'>DesignXcompany</p>
+        <div className={` splash flex flex-col items-center justify-end gap-2 ${showSplash ? '' : 'splashHidden'}`}>
+            <div className='box bg-white h-10 w-10  flex items-center justify-center p-2 gap-2'>
+                <img className='box-text opacity-0 h-6' src="logo.svg" alt="" />
+                <p className='box-text text-center blue-text opacity-0 text-xl bodoni'>DesignXcompany</p>
             </div>
             <h2  className="box box-subtext text-lg life text-center opacity-0 text-white" >where design meets business</h2>
         </div>
