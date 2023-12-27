a7e93cc2d0110e288a52e7e6afe11737e373fd68
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-12-28 02:18:29 +0530

    .

--- app/components/splashscreen.jsx
+++ app/components/splashscreen.jsx
@@ -9,7 +9,7 @@ function SplashScreen() {
 
         // Define the animation timeline
 
-        tl.to('.box', { duration: 0.5, y: -450, ease: 'back.inOut' })
+        tl.to('.box', { duration: 0.5, y: -400, ease: 'back.inOut' })
           .to('.box', { duration:0.5, width:220, ease: 'back.inOut' }) 
           .to('.box-text',{duration:0.5, opacity:1, ease: 'back.inOut'}, "<")
           .to('.box-subtext',{duration:0.5, opacity:1, ease: 'back.inOut'}, ">")
@@ -25,7 +25,7 @@ function SplashScreen() {
         <div className={` splash flex flex-col items-center justify-end ${showSplash ? '' : 'splashHidden'}`}>
             <div className='box bg-white h-12 w-12 rounded-sm flex items-center justify-center p-2'>
                 <img className='box-text opacity-0 h-8' src="logo.svg" alt="" />
-                <p className='box-text text-center blue-text opacity-0 text-lg'>DesignXcompany</p>
+                <p className='box-text text-center blue-text opacity-0 text-lg bodoni'>DesignXcompany</p>
             </div>
             <h2  className="box box-subtext text-lg life text-center opacity-0 text-white" >where design meets business</h2>
         </div>
