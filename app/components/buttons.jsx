f49d2dec9d1da3b74fb023859f6d583410616bd3
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-12-28 03:04:50 +0530

    .

--- app/components/buttons.jsx
+++ app/components/buttons.jsx
@@ -1,8 +1,10 @@
+import { FaExternalLinkAlt } from "react-icons/fa";
+
 function Buttons ({name1 , name2}){
     return <>
     <div className=" flex justify-center gap-4 py-8 my-2 lg:gap-16">
     <button className="button crimson bg-blue-voilet">{name1}</button>
-    <button className="button crimson bg-black ">{name2}</button>
+    <button className="button crimson bg-black flex justify-center">{name2} <FaExternalLinkAlt className="mt-1 ml-1"/></button>
     </div>
     
     </>
