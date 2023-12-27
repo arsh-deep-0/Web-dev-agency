cfa4396a5f6cb4ab6a9382d93bdd69c359408844
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-12-27 21:14:05 +0530

    .

--- app/components/Heading.jsx
+++ app/components/Heading.jsx
@@ -9,54 +9,54 @@ function Heading() {
       let myText = document.querySelectorAll('.text-reveal');
       console.log(myText);
 
-      
-        const text = new SplitType(myText, { types: 'chars, words' });
 
-        gsap.from(text.chars, {
-          scrollTrigger: {
-            trigger: myText,
-            start: 'top 40%',
-           
-          },
-          opacity: 0,
+      const text = new SplitType(myText, { types: 'chars, words' });
 
-          stagger: 0.035,
-          duration: 1,
-          ease: 'ease.out',
-          onComplete: () => {
-            text.revert()
-          },
-        })
-    
+      gsap.from(text.chars, {
+        scrollTrigger: {
+          trigger: myText,
+          start: 'top 40%',
+
+        },
+        opacity: 0,
+
+        stagger: 0.035,
+        duration: 1,
+        ease: 'ease.out',
+        onComplete: () => {
+          text.revert()
+        },
+      })
+
     }
     const addTextAnimation2 = () => {
       let myText = document.querySelectorAll('.text-reveal2');
       console.log(myText);
 
-      
-        const text = new SplitType(myText, { types: 'chars, words' });
 
-        gsap.from(text.chars, {
-          scrollTrigger: {
-            trigger: myText,
-            start: 'top 40%',
-           
-          },
-          opacity: 0,
-          
-          stagger: 0.035,
-          duration:1.05,
-          ease: 'ease.out',
-          onComplete: () => {
-            text.revert()
-          },
-        })
-    
+      const text = new SplitType(myText, { types: 'chars, words' });
+
+      gsap.from(text.chars, {
+        scrollTrigger: {
+          trigger: myText,
+          start: 'top 40%',
+
+        },
+        opacity: 0,
+
+        stagger: 0.035,
+        duration: 1.05,
+        ease: 'ease.out',
+        onComplete: () => {
+          text.revert()
+        },
+      })
+
     }
 
 
-     addTextAnimation(); 
-     addTextAnimation2();
+    addTextAnimation();
+   
 
   }, [])
 
@@ -65,9 +65,10 @@ function Heading() {
     <>
       <div className=' z-10 relative flex justify-center  items-center py-6'>
         <p id='target' className=' target px-2 aoboshi heading text-3xl text-center font-normal md:text-5xl lg:text-6xl '>
-          <span className='text-reveal target' >A complete web solution to all </span> 
-          <span className='text-reveal2 blue-text pr-2 target'>Home Remodelling & Construction Businesses </span>
-          </p>
+          <span className='text-reveal target' >A complete web solution to all </span>
+          <span className='text-reveal bluee-text pr-2 target'>Home Remodelling & Construction Businesses </span>
+        </p>
+
       </div>
 
     </>
